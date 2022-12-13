f8920ecda77d369a6fd56a91c3bc95d2cac87d2c
Eunjae TonyLee <lpl2001@naver.com>
2022-12-13 15:32:18 +0900

    some css added 4

--- src/pages/Name.js
+++ src/pages/Name.js
@@ -5,7 +5,7 @@ const Name = () => {
   return (
     <div className="Name-wrapper">
       <div className="Name-content-container">
-        <form>
+        <form className="Name-input-container">
           <input className="Name-input" placeholder="이름을 입력하세요"></input>
           <Button to="/situation">시작하기</Button>
         </form>
