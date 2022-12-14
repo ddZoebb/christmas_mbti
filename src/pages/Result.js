21c8403059765ae8377bab47d6c9acbd029be8b5
Eunjae TonyLee <lpl2001@naver.com>
2022-12-15 00:33:38 +0900

    result page commit

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,18 +1,26 @@
 import React from "react";
 
-function Result(){
-  return(
+function Result() {
+  return (
     <div className="Result-wrapper">
       <div className="Result-card">
         <div className="Result-img-container">
-          <img className="Result-img" alt="결과이미지" src="img\INTJ.png"></img>
+          <img className="Result-img" alt="결과이미지" src="img\ISFP.png"></img>
         </div>
         <div className="Result-content-container">
-          <div className="Result-title">INTJ</div>
+          <div className="Result-box">
+            <div className="Result-prefix">스키를 즐기는</div>
+            <div className="Result-title">코코넛</div>
+          </div>
+          <div className="Result-text-box">
+            <div className="Result-text-title"></div>
+            <div className="Result-text-content"></div>
+          </div>
+
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Result;
