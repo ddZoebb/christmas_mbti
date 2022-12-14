fd11ba7367ce08311e2e6238e3ecd9723d9a2519
Eunjae TonyLee <lpl2001@naver.com>
2022-12-14 23:32:29 +0900

    result page design

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function Result(){
   return(
-    <div>
-      
+    <div className="Result-wrapper">
+      <div className="Result-card">
+        <div className="Result-img-container">
+          <img className="Result-img" alt="결과이미지" src="img\INTJ.png"></img>
+        </div>
+        <div className="Result-content-container">
+          <div className="Result-title">INTJ</div>
+        </div>
+      </div>
     </div>
   )
 }
