a13a6260f1cf951df81f2e5eaf44dba5c4bb9d19
Eunjae TonyLee <lpl2001@naver.com>
2022-12-15 19:29:59 +0900

    result page commit

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -13,10 +13,36 @@ function Result() {
             <div className="Result-title">코코넛</div>
           </div>
           <div className="Result-text-box">
-            <div className="Result-text-title"></div>
-            <div className="Result-text-content"></div>
+            <div className="Result-text-title">당신의 성격은</div>
+            <div className="Result-text-content">
+              이러이러합니다. 이러이러합니다. 이러이러합니다. 이러이러합니다.
+              이러이러합니다. 이러이러합니다.
+            </div>
           </div>
-
+          <div className="Result-text-box likes">
+            <div className="Result-text-title">이런 것을 좋아해요</div>
+            <div className="Result-text-content">
+              이러이러합니다. 이러이러합니다. 이러이러합니다. 이러이러합니다.
+              이러이러합니다. 이러이러합니다.
+            </div>
+          </div>
+          <div className="Result-text-box dislikes">
+            <div className="Result-text-title">이런 것은 싫어해요</div>
+            <div className="Result-text-content">
+              이러이러합니다. 이러이러합니다. 이러이러합니다. 이러이러합니다.
+              이러이러합니다. 이러이러합니다.
+            </div>
+          </div>
+          <div className="Result-relation-box">
+            <div className="Result-relation-container">
+              <div className="Result-relation-title">잘 맞는 친구들</div>
+              <div className="Result-relation-content">가나다 마바사</div>
+            </div>
+            <div className="Result-relation-container">
+              <div className="Result-relation-title">안 맞는 친구들</div>
+              <div className="Result-relation-content">아자차 카타파</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
